182f70013b4208a3554c8d57a980c2829e5df092
Nayeem <nayeem8527@users.noreply.github.com>
2016-09-28 14:26:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick = function(){
 	};	
 
 	//Make a request
-	request.open("GET","http://192.168.100.3:8080/counter",true);
+	request.open("GET","http://nayeem8527.imad.hasura-app.io/counter",true);
 	request.send(null);
 };
 
