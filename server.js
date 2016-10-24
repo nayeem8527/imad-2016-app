fca84c5a4a5130fa98f6ed42914b8f8642ad7b69
Nayeem <nayeem8527@users.noreply.github.com>
2016-10-24 23:49:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/submit-name', function(req,res){ //URL = /submit-name?name=sdvfd
 
 app.get('/articles/:articleName', function (req, res) {
   
-  pool.query("SELCT * FROM article WHERE title = "+ req.params.articleName,function(err,result){
+  pool.query("SELECT * FROM article WHERE title = "+ req.params.articleName,function(err,result){
       if(err){
           res.status(500).send(err.toString());
       }else{
