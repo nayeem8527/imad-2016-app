8d52778851441ebe67ece853cb6d153b20cd165a
Nayeem <nayeem8527@users.noreply.github.com>
2016-11-05 21:53:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,4 @@
-// Counter code
-
-var button = document.getElementById("counter");
-button.onclick = function(){
-
-	//Create a request
-	var request = new XMLHttpRequest();
-
-	//Capture the response and store it in the variable
-	request.onreadystatechange = function(){
-		if(request.readyState === XMLHttpRequest.DONE){
-			//Take some action
-			if(request.status === 200){
-				var counter = request.responseText;
-				var span = document.getElementById("count");
-				span.innerHTML = counter.toString();
-			}
-		}
-		//Not done yet
-	};	
-
-	//Make a request
-	request.open("GET","http://nayeem8527.imad.hasura-app.io/counter",true);
-	request.send(null);
-};
-
-//Submit name
+//Submit username and password to login
 var submit = document.getElementById("submit_btn");
 submit.onclick = function(){
 
@@ -36,25 +10,21 @@ submit.onclick = function(){
 		if(request.readyState === XMLHttpRequest.DONE){
 			//Take some action
 			if(request.status === 200){
-				//Capture the list of names and render it as a list
-				var names = request.responseText;
-				names = JSON.parse(names);
-				var list = "";
-				for(var i=0;i<names.length;i++){
-					list += "<li>" + names[i] + "</li>";
-				}
-				var ul = document.getElementById("namelist");
-				ul.innerHTML = list;
+				console.log('user is logged in');
+				alert('Logged in successfully');
+			}else{
+			    alert('Username/Password is incorrect');
 			}
 		}
 		//Not done yet
 	};	
 
 	//Make a request
-	var nameInput= document.getElementById("name");
-	var name = nameInput.value;
-	request.open("GET","http://nayeem8527.imad.hasura-app.io/submit-name?name="+name,true);
-	request.send(null);
+	var username= document.getElementById("username").value;
+	var password= document.getElementById("password").value;
+	request.open("POST","http://nayeem8527.imad.hasura-app.io/login",true);
+	request.setRequestHeader('Content-Type','application/json');
+	request.send(JSON.stringify({username:username,password:password}));
 	//Make a request to the server and send the name
 	
 };
