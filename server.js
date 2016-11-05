1cf65218e305e0caf934298b318916fc530fab61
Nayeem <nayeem8527@users.noreply.github.com>
2016-11-05 22:03:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user:'nayeem8527',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret:'soneRandomSecretValue',
+    cookie: {maxAge : 1000 * 60 * 60 * 24 * 30}
+}));
 
 function createTemplate(data){
     var title = data.title;
@@ -105,6 +110,12 @@ app.post('/login',function(req,res){
               var salt = dbString.split('$')[2];
               var hashedPassword = hash(password,salt);
               if(hashedPassword === dbString){
+                  
+                    // set the session
+                    req.session.auth = {userId: result.rows[0].id};
+                    //set cookie with a session id
+                    //internally on the server side it maps the seddio id with object
+                  
                     res.send('credential correct!');       
               }
               else{
@@ -113,6 +124,15 @@ app.post('/login',function(req,res){
           }
       } 
    });
+});
+
+app.get('/check-login',function(req,res){
+    if(req.session && req.session.auth && req.session.auth.userId){
+        res.send('You are logged in :' + req.session.auth.userId.toString());
+    }
+    else{
+        res.send('You are not logged in');
+    }
 })
 
 var pool = new Pool(config)
